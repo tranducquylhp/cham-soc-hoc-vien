54cea857e60933024092348515a4981a6464aae6
tranducquylhp <tranducquylhp@gmail.com>
2022-04-02 21:08:24 +0700

    init

--- src/main/java/com/codegym/demo_chatbot_fb/model/Student.java
+++ src/main/java/com/codegym/demo_chatbot_fb/model/Student.java
@@ -24,4 +24,92 @@ public class Student {
     private Long classNumber;
     private String schedule;
     private Date createDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getParentPhoneNumber() {
+        return parentPhoneNumber;
+    }
+
+    public void setParentPhoneNumber(String parentPhoneNumber) {
+        this.parentPhoneNumber = parentPhoneNumber;
+    }
+
+    public Date getDateRegister() {
+        return dateRegister;
+    }
+
+    public void setDateRegister(Date dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
+    public Long getMonthRegister() {
+        return monthRegister;
+    }
+
+    public void setMonthRegister(Long monthRegister) {
+        this.monthRegister = monthRegister;
+    }
+
+    public Date getDateExpired() {
+        return dateExpired;
+    }
+
+    public void setDateExpired(Date dateExpired) {
+        this.dateExpired = dateExpired;
+    }
+
+    public Long getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(Long classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 }
